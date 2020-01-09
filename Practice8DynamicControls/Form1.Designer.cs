3d3a81388a2931b71c693c0bb9397d26454f1baa
Morris Mulitu <37964707+MorrisMuuoMulitu@users.noreply.github.com>
2020-01-09 17:40:41 +0100

    Add files via upload

---
+++ Practice8DynamicControls/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Practice8DynamicControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtNumberValues = new System.Windows.Forms.TextBox();
+            this.BtnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of values";
+            // 
+            // TxtNumberValues
+            // 
+            this.TxtNumberValues.Location = new System.Drawing.Point(192, 94);
+            this.TxtNumberValues.Name = "TxtNumberValues";
+            this.TxtNumberValues.Size = new System.Drawing.Size(100, 20);
+            this.TxtNumberValues.TabIndex = 1;
+            // 
+            // BtnGenerate
+            // 
+            this.BtnGenerate.Location = new System.Drawing.Point(397, 94);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.BtnGenerate.TabIndex = 2;
+            this.BtnGenerate.Text = "Generate";
+            this.BtnGenerate.UseVisualStyleBackColor = true;
+            this.BtnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 450);
+            this.Controls.Add(this.BtnGenerate);
+            this.Controls.Add(this.TxtNumberValues);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtNumberValues;
+        private System.Windows.Forms.Button BtnGenerate;
+    }
+}
+
---
+++ Practice8DynamicControls/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice8DynamicControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            this.AutoSize = true;
+            this.Padding = new Padding(0, 0, 20, 20);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+        private List<TextBox> inputTextBoxes;
+
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            //Get the number of input textBoxes.
+            int inputNumber = int.Parse(TxtNumberValues.Text);
+            //Initialize List of input textboxes
+            inputTextBoxes = new List<TextBox>();
+            //Generate labels and textboxes
+            for (int i = 1; i < inputNumber; i++)
+            {
+                Label inputLabel = new Label();
+                TextBox inputTextBox = new TextBox();
+
+                //Initialize label
+                inputLabel.Text = "Input " + i;
+                inputLabel.Location = new Point(30, TxtNumberValues.Bottom + (i * 30));
+                inputLabel.AutoSize = true;
+
+                //Initialize text boxes properties.
+                inputTextBox.Location = new Point(inputLabel.Width, inputLabel.Top - 3);
+                //Add the newly created text box to the list of input text boxes.
+                inputTextBoxes.Add(inputTextBox);
+                //Add the labels and text box to the form
+                this.Controls.Add(inputTextBox);
+                this.Controls.Add(inputTextBox);
+                this.CenterToScreen();
+
+            }
+            //Create an Add button
+            Button BtnAdd = new Button();
+            //Initialize properties
+            BtnAdd.Text = "Add";
+            //Make the button show up in the middle of the form and right below the last input box
+            BtnAdd.Location = new Point(this.Width / 2 - BtnAdd.Width / 2, inputTextBoxes[inputTextBoxes.Count - 1].Bottom + 20);
+            //Add an event handler to the button's click event.
+            BtnAdd.Click += new EventHandler(BtnAdd_Click);
+            //Add the button to the form
+            this.Controls.Add(BtnAdd);
+            //Center the form to the screen
+            this.CenterToScreen();
+
+           
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            foreach (TextBox textBoxItem in inputTextBoxes)
+            {
+                if (textBoxItem.Text==string.Empty)
+                {
+                    MessageBox.Show("Please fill in all the text boxes! ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                else
+                {
+                    sum += int.Parse(textBoxItem.Text);
+                }
+            }
+            MessageBox.Show("The sum is : " + sum); 
+        }
+    }
+}
