27e282a1aa08a70cdcba0631eb238eaa85cd34de
Morris Mulitu <37964707+MorrisMuuoMulitu@users.noreply.github.com>
2020-01-09 17:23:43 +0100

    Add files via upload

---
+++ Pizza/PizzaSimple/PizzaSimple/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace PizzaSimple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblSmall = new System.Windows.Forms.Label();
+            this.LblLarge = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdBtn1Large = new System.Windows.Forms.RadioButton();
+            this.rdBtn1Small = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rdBtn2Large = new System.Windows.Forms.RadioButton();
+            this.rdBtn2Small = new System.Windows.Forms.RadioButton();
+            this.TxtPizza1 = new System.Windows.Forms.TextBox();
+            this.TxtPizza2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtPayable = new System.Windows.Forms.TextBox();
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnLoadData = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LblSmall
+            // 
+            this.LblSmall.AutoSize = true;
+            this.LblSmall.Location = new System.Drawing.Point(244, 67);
+            this.LblSmall.Name = "LblSmall";
+            this.LblSmall.Size = new System.Drawing.Size(46, 13);
+            this.LblSmall.TabIndex = 0;
+            this.LblSmall.Text = "LblSmall";
+            // 
+            // LblLarge
+            // 
+            this.LblLarge.AutoSize = true;
+            this.LblLarge.Location = new System.Drawing.Point(511, 79);
+            this.LblLarge.Name = "LblLarge";
+            this.LblLarge.Size = new System.Drawing.Size(48, 13);
+            this.LblLarge.TabIndex = 1;
+            this.LblLarge.Text = "LblLarge";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(83, 134);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(83, 205);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdBtn1Large);
+            this.groupBox1.Controls.Add(this.rdBtn1Small);
+            this.groupBox1.Location = new System.Drawing.Point(247, 134);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 50);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose";
+            // 
+            // rdBtn1Large
+            // 
+            this.rdBtn1Large.AutoSize = true;
+            this.rdBtn1Large.Location = new System.Drawing.Point(244, 14);
+            this.rdBtn1Large.Name = "rdBtn1Large";
+            this.rdBtn1Large.Size = new System.Drawing.Size(83, 17);
+            this.rdBtn1Large.TabIndex = 1;
+            this.rdBtn1Large.TabStop = true;
+            this.rdBtn1Large.Text = "rdBtn1Large";
+            this.rdBtn1Large.UseVisualStyleBackColor = true;
+            // 
+            // rdBtn1Small
+            // 
+            this.rdBtn1Small.AutoSize = true;
+            this.rdBtn1Small.Location = new System.Drawing.Point(6, 28);
+            this.rdBtn1Small.Name = "rdBtn1Small";
+            this.rdBtn1Small.Size = new System.Drawing.Size(81, 17);
+            this.rdBtn1Small.TabIndex = 0;
+            this.rdBtn1Small.TabStop = true;
+            this.rdBtn1Small.Text = "rdBtn1Small";
+            this.rdBtn1Small.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rdBtn2Large);
+            this.panel1.Controls.Add(this.rdBtn2Small);
+            this.panel1.Location = new System.Drawing.Point(221, 215);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 37);
+            this.panel1.TabIndex = 6;
+            // 
+            // rdBtn2Large
+            // 
+            this.rdBtn2Large.AutoSize = true;
+            this.rdBtn2Large.Location = new System.Drawing.Point(267, 4);
+            this.rdBtn2Large.Name = "rdBtn2Large";
+            this.rdBtn2Large.Size = new System.Drawing.Size(83, 17);
+            this.rdBtn2Large.TabIndex = 1;
+            this.rdBtn2Large.TabStop = true;
+            this.rdBtn2Large.Text = "rdBtn2Large";
+            this.rdBtn2Large.UseVisualStyleBackColor = true;
+            // 
+            // rdBtn2Small
+            // 
+            this.rdBtn2Small.AutoSize = true;
+            this.rdBtn2Small.Location = new System.Drawing.Point(26, 12);
+            this.rdBtn2Small.Name = "rdBtn2Small";
+            this.rdBtn2Small.Size = new System.Drawing.Size(81, 17);
+            this.rdBtn2Small.TabIndex = 0;
+            this.rdBtn2Small.TabStop = true;
+            this.rdBtn2Small.Text = "rdBtn2Small";
+            this.rdBtn2Small.UseVisualStyleBackColor = true;
+            // 
+            // TxtPizza1
+            // 
+            this.TxtPizza1.Location = new System.Drawing.Point(731, 152);
+            this.TxtPizza1.Name = "TxtPizza1";
+            this.TxtPizza1.Size = new System.Drawing.Size(100, 20);
+            this.TxtPizza1.TabIndex = 7;
+            // 
+            // TxtPizza2
+            // 
+            this.TxtPizza2.Location = new System.Drawing.Point(731, 215);
+            this.TxtPizza2.Name = "TxtPizza2";
+            this.TxtPizza2.Size = new System.Drawing.Size(100, 20);
+            this.TxtPizza2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(254, 375);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Payable";
+            // 
+            // TxtPayable
+            // 
+            this.TxtPayable.Location = new System.Drawing.Point(323, 375);
+            this.TxtPayable.Name = "TxtPayable";
+            this.TxtPayable.ReadOnly = true;
+            this.TxtPayable.Size = new System.Drawing.Size(100, 20);
+            this.TxtPayable.TabIndex = 10;
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.Location = new System.Drawing.Point(83, 375);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.BtnCalculate.TabIndex = 11;
+            this.BtnCalculate.Text = "Calculate";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(83, 477);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(76, 23);
+            this.BtnClear.TabIndex = 12;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnLoadData
+            // 
+            this.BtnLoadData.Location = new System.Drawing.Point(423, 477);
+            this.BtnLoadData.Name = "BtnLoadData";
+            this.BtnLoadData.Size = new System.Drawing.Size(62, 29);
+            this.BtnLoadData.TabIndex = 13;
+            this.BtnLoadData.Text = "LoadData";
+            this.BtnLoadData.UseVisualStyleBackColor = true;
+            this.BtnLoadData.Click += new System.EventHandler(this.BtbLoadData_Click);
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Location = new System.Drawing.Point(840, 483);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(75, 23);
+            this.BtnClose.TabIndex = 14;
+            this.BtnClose.Text = "Close";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(856, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "pieces";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(856, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "pieces";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 576);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.BtnLoadData);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.BtnCalculate);
+            this.Controls.Add(this.TxtPayable);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtPizza2);
+            this.Controls.Add(this.TxtPizza1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.LblLarge);
+            this.Controls.Add(this.LblSmall);
+            this.Name = "Form1";
+            this.Text = "Pizza Simplified";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblSmall;
+        private System.Windows.Forms.Label LblLarge;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdBtn1Large;
+        private System.Windows.Forms.RadioButton rdBtn1Small;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rdBtn2Large;
+        private System.Windows.Forms.RadioButton rdBtn2Small;
+        private System.Windows.Forms.TextBox TxtPizza1;
+        private System.Windows.Forms.TextBox TxtPizza2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtPayable;
+        private System.Windows.Forms.Button BtnCalculate;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Button BtnLoadData;
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Pizza/PizzaSimple/PizzaSimple/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PizzaSimple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            openFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog1.FileName = "";
+        }
+        private int sizeSmall = 32, sizeLarge = 45;
+        private List<Pizza> pizzas = new List<Pizza>();
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            visibilitySet(false);
+        }
+
+        private void visibilitySet(bool visibleOrNot)
+        {
+            foreach (Control item in this.Controls)
+            {
+                item.Visible = visibleOrNot;
+            }
+            BtnLoadData.Visible = !visibleOrNot;
+        }
+
+        private void BtbLoadData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (InputData())
+                {
+                    DisplayControls();
+                    visibilitySet(true);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private bool InputData()
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            bool success = false;
+            if (result==DialogResult.OK)
+            {
+                StreamReader reader = null;
+                try
+                {
+                    string filename = openFileDialog1.FileName;
+                    reader = new StreamReader(filename);
+                    ReadingData(reader);
+                    success = true;
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("There is an error when we read the file", "Error");
+                    success = false;
+                }
+                finally
+                {
+                    if (reader!=null)
+                    {
+                        reader.Close();
+                    }
+                }
+            }
+            return success;
+        }
+
+        private void ReadingData(StreamReader reader)
+        {
+            string dataLine;
+            while (!reader.EndOfStream)
+            {
+                dataLine = reader.ReadLine();
+                string[] data = dataLine.Split(';');
+                pizzas.Add(new Pizza(data[0], int.Parse(data[1]), int.Parse(data[2])));
+            }
+        }
+
+        
+
+        private void DisplayControls()
+        {
+            LblSmall.Text = sizeSmall + " cm ";
+            LblLarge.Text = sizeLarge+ " cm ";
+            checkBox1.Text = pizzas[0].Name;
+            rdBtn1Small.Text = pizzas[0].SmallSizePrice + " Ft ";
+            rdBtn1Large.Text = pizzas[0].LargeSizePrice + " Ft ";
+            checkBox2.Text = pizzas[1].Name;
+            rdBtn2Small.Text = pizzas[1].SmallSizePrice + " Ft ";
+            rdBtn2Large.Text = pizzas[1].LargeSizePrice + " Ft ";
+           
+            
+        }
+
+        
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int sum = 0, price = 0, piece = 0;
+                //if the user chooses the first pizza
+                if (checkBox1.Checked)
+                {
+                    if (rdBtn1Small.Checked) price = pizzas[0].SmallSizePrice;
+                    else if (rdBtn1Large.Checked) price = pizzas[0].LargeSizePrice;
+                    else throw new MissingFieldException("Choose size!");
+                    piece = int.Parse(TxtPizza1.Text);
+                    if (piece <= 0) throw new ArgumentOutOfRangeException("Not a negative value");
+                    sum += price * piece;
+                }
+                //if the user selects the second checkbox
+                if (checkBox2.Checked)
+                {
+                    if (rdBtn2Small.Checked) price = pizzas[1].SmallSizePrice;
+                    else if (rdBtn2Large.Checked) price = pizzas[1].LargeSizePrice;
+                    else throw new MissingFieldException("Choose a size");
+                    piece = int.Parse(TxtPizza2.Text);
+                    if (piece <= 0) throw new ArgumentOutOfRangeException("Do not give a negative value");
+                    sum += price * piece;
+                }
+                if (!checkBox1.Checked && !checkBox2.Checked) throw new MissingFieldException("Choose a pizza");
+                TxtPayable.Text = sum + "Ft";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("The number of pieces of pizza is a wrong value");
+            }
+            catch(ArgumentOutOfRangeException aox)
+            {
+                MessageBox.Show(aox.Message, "Error");
+            }
+            catch(MissingFieldException mex)
+            {
+                MessageBox.Show(mex.Message, "Error");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Some error was", "Error");
+            }
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item is CheckBox) (item as CheckBox).Checked = false;
+                if (item is TextBox) (item as TextBox).Clear();
+                if (item.HasChildren)
+                {
+                    foreach(Control child in item.Controls)
+                    {
+                        if (child is RadioButton) (child as RadioButton).Checked = false;
+                    }
+                }
+            }
+        }
+    }
+}
