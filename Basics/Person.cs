62ce8601fcca3658f98c531e8acf27d79555ebc3
Morris Mulitu <37964707+MorrisMuuoMulitu@users.noreply.github.com>
2020-01-09 17:06:52 +0100

    Add files via upload

---
+++ Basics/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    class Person
+    {
+        public string FirstName { get;  set; }
+        public string LastName { get;  set; }
+
+        public Person(string firstName, string lastName)
+        {
+            this.LastName = lastName;
+            this.FirstName = firstName;
+        }
+
+        public Person()
+        {
+        }
+
+        //Methods
+        public void GetFullName()
+        {
+            Console.WriteLine("Get my full name {0} {1}.", FirstName, LastName);
+        }
+        public void Walk()
+        {
+            Console.WriteLine("I am walking");
+        }
+        public void Talk()
+        {
+            Console.WriteLine("I am talking");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("I am eating");
+        }
+    }
+}
---
+++ Basics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person me = new Person();
+            me.FirstName = "Morris";
+            me.LastName = "Muuo";
+
+            me.GetFullName();
+            me.Talk();
+            me.Walk();
+
+            SuperPerson superPerson = new SuperPerson();
+            superPerson.FirstName = "Super";
+            superPerson.LastName = "Dude";
+
+            superPerson.GetFullName();
+            superPerson.Talk();
+            superPerson.Walk();
+
+
+        }
+    }
+}
